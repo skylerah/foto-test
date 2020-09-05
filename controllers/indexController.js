92dc96dc7af896de545c154349b52f467ea57930
Temisan Iwere <temisan.iwere@publicissapient.com>
2020-09-05 10:54:56 -0400

    cleanup

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const keys = require("../config/keys.json");
 require("dotenv").config();
 
 const app = express();
