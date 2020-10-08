63f873b8cec6961cf46bf89509d700ce907a9e72
Temisan Iwere <temisan.iwere@publicissapient.com>
2020-10-08 10:22:00 -0400

    changed user schema structure

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
 var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    images: {type: Array, required: false},
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
 });
 
 var User = mongoose.model("users", UserSchema);
